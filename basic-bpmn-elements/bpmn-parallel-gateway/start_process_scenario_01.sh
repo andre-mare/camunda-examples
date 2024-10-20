8baa177ae89b7624e99ffadf915be232e161e931
Andre <andre.mare@gmail.com>
2024-10-20 10:30:44 +0200

    Add BPMN Parallel Gateway Example

---
+++ basic-bpmn-elements/bpmn-parallel-gateway/start_process_scenario_01.sh
@@ -0,0 +1,2 @@
+echo "\nOrder Coffee"
+curl --location --request POST 'http://localhost:8080/engine-rest/process-definition/key/order-coffee/start' --header 'Content-Type: application/json' --data-raw '{}'
